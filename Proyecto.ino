251403ebf28a2f48e1b7ba2d9060fdc63fc9c077
Saúl Chávez <70542967+reknoblothbrok@users.noreply.github.com>
2020-11-30 17:54:49 -0600

    Update Proyecto.ino
    El proyecto es una llenadora de solidos, donde cada LED representa una parte del proceso y las entradas pueden ser desde sensores hasta botones.

--- Proyecto.ino
+++ Proyecto.ino
@@ -1,35 +1,45 @@
 ///////////// Keypad ////////////////////
 #include <Keypad.h>
 
-const byte FILAS = 4;
-const byte COLUMNAS = 3;
+const byte FILAS = 2;
+const byte COLUMNAS = 2;
 
 char keys[FILAS][COLUMNAS] = {
-  { '1', '2', '3'},
-  { '4', '5', '6'},
-  { '7', '8', '9'},
-  { '*', '0', '#'}
+  { '1', '2'},
+  { '4', '5'},
 };
-byte pinesFilas[FILAS] = { 11, 10, 9, 8};
-byte pinesColumnas[COLUMNAS] = { 7, 6, 5};
+byte pinesFilas[FILAS] = { 11, 10};
+byte pinesColumnas[COLUMNAS] = { 7, 6};
 
 Keypad teclado = Keypad(makeKeymap(keys), pinesFilas, pinesColumnas, FILAS, COLUMNAS);
 
 char TECLA;       // almacena la tecla presionada
-// 1 = Bóton de arranque
-// 2 = Botón de paro de emergencia
-// 3 = Botón de paro
-// 4 = Sensor de bolsa (Capasitivo)
-// 5 = Bolsa de 50g
-// 6 = Bolsa de 150g
-// 7 = Bolsa de 300g
-// 8 = Bolsa de 500g
+// 1 = Bolsa de 50g
+// 2 = Bolsa de 150g
+// 4 = Bolsa de 300g
+// 5 = Bolsa de 500g
 /////////////////// Salidas ///////////////
-int LED1 = 2;     // LED 1, indica que comenzó el proceso
-int LED2 = 3;     // LED 2, le manda señal a la banda transportadora
-int LED3 = 12;    // LED 3, Señal de alerta
-int LED4 = 13;    // LED 4, Resistencia para sellar y cortar la bolsa
-int LED5 = 4;     // LED 5, Caudal
+int LED1 = 2;     //  LED 1, indica que ocmenzó el proceso
+int LED2 = 3;     // LED 2, le manda señal a la banda transportadora pin 3
+int LED3 = 12;    // LED 3, Señal de alerta pin 4
+int LED4 = 4;    // LED 4, Resistencia para sellar y cortar la bolsa pin 5
+int LED5 = 5;     // LED 5, Caudal pin 6
+
+////////////////// Botones o sensores ///////////
+int S_C = 8;      // Sensor Capacitivo (Detecta si hay o no bolsas)
+int SNC = LOW;    //
+int B_A = 13;      // Botón de arranque
+int ba = LOW;
+int B_P = 9;      // Botón de paro
+int bp = LOW;
+/////////////// Pot  ////////////////
+long pot = A0;      // Potenciometro para indicar cuantas veces se ciclará
+long valor;         // Lectura del Pot
+int valor2;         // Escala del pot de 0 a 100
+
+long pot2 = A1;     // Pot que representa la distancia (Sensor ultrasonico)
+long d;             // Lectura de distancia
+float d2;             // Escala de distancia
 
 void setup()
 {
@@ -39,71 +49,189 @@ void setup()
   pinMode(LED3, OUTPUT);
   pinMode(LED4, OUTPUT);
   pinMode(LED5, OUTPUT);
-  pinMode('4', INPUT);
+  pinMode(S_C, INPUT);
+  pinMode(B_A, INPUT);
+  pinMode(B_P, INPUT);
+
+  
 }
 void loop()
 {
+  bp = digitalRead(B_P);
+
+  if(bp != HIGH)
+  {
+  d = analogRead(pot2);
+  d2 = (d * 10)/1024;
+  Serial.print("El nivel del tanque es: ");
+  Serial.println(d2);
+  if(d2 > 1)
+  {
+  ba = digitalRead(B_A);
+  if(ba == HIGH)
+  {
+  digitalWrite(LED1, HIGH);    // Indica que la maquina está encendida
+
+  valor = analogRead(pot);
+  valor2 = (valor * 100)/1024;
+  Serial.print("La cantidad de veces que se debe ciclar son: ");
+  Serial.println(valor2 + 1);
+  delay(1000);
   TECLA = teclado.getKey();    // Obtiene tecla presionada y asigna a variable
   switch(TECLA)
   {
-    case '1':                     // Arranque del programa
-      digitalWrite(LED1, HIGH);   // Enciende LED que indica el inicio del proceso
-      digitalWrite(LED3, LOW);
-      break;
-        case '2':                    // Botón de Paro
-          digitalWrite(LED1, LOW);
-          digitalWrite(LED2, LOW);
-          digitalWrite(LED3, HIGH);
-          digitalWrite(LED4, LOW);
+        case '1':                     // Se trabaja con las bolsas de 50g
+           valor = analogRead(pot);
+           valor2 = (valor * 100)/1024;
+           for(int i = 0; i <= valor2; i++)
+           {
+           SNC = digitalRead(S_C);
+           
+           if(SNC == HIGH)
+           {
+             
+             digitalWrite(LED4, HIGH);   // Sella la bolsa 
+             delay(2000);                // El sellado permanece por 2s
+             digitalWrite(LED4, LOW);    // Desactiva el sellado
+             digitalWrite(LED2, HIGH);   // Se enciende la banda transportadora
+             delay(1000);                // La banda transportadora avanza durante 1 segundo, arrastrando la bolsa a la medida deseada
+             digitalWrite(LED2, LOW);
+             digitalWrite(LED5, HIGH);   // Se abre el recipiente
+             delay(2000);                // El caudal dura abierto 2s
+             digitalWrite(LED5, LOW);    // Se cierra el Caudal
+             }
+           else                        // Enciende Alerta por falta de bolsas
+           {
+            for(int i = 1; i <= 10; i++)   //Parpadea el LED de alarma
+            {
+             digitalWrite(LED3, HIGH);
+             delay(1000);
+             digitalWrite(LED3, LOW);
+             delay(1000);
+            }
+           }
+             }
           break;
-        case '5':                     // Se trabaja con las bolsas de 50g
-
-          digitalWrite(LED4, HIGH);   // Sella la bolsa 
-          delay(2000);                // El sellado permanece por 2s
-          digitalWrite(LED4, LOW);    // Desactiva el sellado
-          digitalWrite(LED2, HIGH);   // Se enciende la banda transportadora
-          delay(1000);                // La banda transportadora avanza durante 1 segundo, arrastrando la bolsa a la medida deseada
-          digitalWrite(LED5, HIGH);   // Se abre el recipiente
-          delay(2000);                // El caudal dura abierto 2s
-          digitalWrite(LED5, LOW);    // Se cierra el Caudal
-
-          break;
-         case '6':                     // Se trabaja con las bolsa de 150g
-
+         case '2':                     // Se trabaja con las bolsa de 150g
+             valor = analogRead(pot);
+             valor2 = (valor * 100)/1024;
+             for(int i = 0; i <= valor2; i++)
+             {
+          SNC = digitalRead(S_C);
+          if(SNC == HIGH)
+          {
+             
           digitalWrite(LED4, HIGH);   // Sella la bolsa 
           delay(2000);                // El sellado permanece por 2s
           digitalWrite(LED4, LOW);    // Desactiva el sellado
           digitalWrite(LED2, HIGH);   // Se enciende la banda transportadora
           delay(3000);                // La banda transportadora avanza durante 3 segundos, arrastrando la bolsa a la medida deseada
+          digitalWrite(LED2, LOW);
           digitalWrite(LED5, HIGH);   // Se abre el recipiente
           delay(3500);                // El caudal dura abierto 3.5s
           digitalWrite(LED5, LOW);    // Se cierra el Caudal
- 
+          }
+          
+          else                        // Enciende Alerta por falta de bolsas
+          {
+            for(int i = 1; i <= 10; i++)   //Parpadea el LED de alarma
+            {
+            digitalWrite(LED3, HIGH);
+            delay(1000);
+            digitalWrite(LED3, LOW);
+            delay(1000);
+            }
+          }
+             }
           break;
-         case '7':                     // Se trabaja con las bolsa de 300g
+         case '4':                     // Se trabaja con las bolsa de 300g
+             valor = analogRead(pot);
+             valor2 = (valor * 100)/1024;
 
+             for(int i = 0; i <= valor2; i++)
+             {
+          SNC = digitalRead(S_C);
+          if(SNC == HIGH)
+          {
+            
           digitalWrite(LED4, HIGH);   // Sella la bolsa 
           delay(2000);                // El sellado permanece por 2s
           digitalWrite(LED4, LOW);    // Desactiva el sellado
           digitalWrite(LED2, HIGH);   // Se enciende la banda transportadora
           delay(6000);                // La banda transportadora avanza durante 6 segundos, arrastrando la bolsa a la medida deseada
+          digitalWrite(LED2, LOW);
           digitalWrite(LED5, HIGH);   // Se abre el recipiente
           delay(6000);                // El caudal dura abierto 6s
           digitalWrite(LED5, LOW);    // Se cierra el Caudal
-
+          }
+          
+          else                        // Enciende Alerta por falta de bolsas
+          {
+            for(int i = 1; i <= 10; i++)   //Parpadea el LED de alarma
+            {
+            digitalWrite(LED3, HIGH);
+            delay(1000);
+            digitalWrite(LED3, LOW);
+            delay(1000);
+            }
+          }
+             }
           break;
-         case '8':                     // Se trabaja con las bolsa de 500g
+         case '5':                     // Se trabaja con las bolsa de 500g
+             valor = analogRead(pot);
+             valor2 = (valor * 100)/1024;
 
+             for(int i = 0; i <= valor2; i++)
+             {
+          SNC = digitalRead(S_C);
+          if(SNC == HIGH)
+          {
+            
           digitalWrite(LED4, HIGH);   // Sella la bolsa 
           delay(2000);                // El sellado permanece por 2s
           digitalWrite(LED4, LOW);    // Desactiva el sellado
           digitalWrite(LED2, HIGH);   // Se enciende la banda transportadora
           delay(10000);                // La banda transportadora avanza durante 10 segundos, arrastrando la bolsa a la medida deseada
+          digitalWrite(LED2, LOW);
           digitalWrite(LED5, HIGH);   // Se abre el recipiente
           delay(8000);                // El caudal dura abierto 8s
           digitalWrite(LED5, LOW);    // Se cierra el Caudal
-
+          }
+          
+          else                        // Enciende Alerta por falta de bolsas
+          {
+            for(int i = 1; i <= 10; i++)   //Parpadea el LED de alarma
+            {
+            digitalWrite(LED3, HIGH);
+            delay(1000);
+            digitalWrite(LED3, LOW);
+            delay(1000);
+            }
+          }
+             }
           break;
       }
-
-  } 
+  }
+  else
+ {
+  digitalWrite(LED1, LOW);
+ }
+  }
+  else
+  {
+    digitalWrite(LED1, LOW);
+    for(int i = 1; i <= 10; i++)   //Parpadea el LED de alarma
+            {
+            digitalWrite(LED3, HIGH);
+            delay(1000);
+            digitalWrite(LED3, LOW);
+            delay(1000);
+            }
+    }
+ }
+ else
+ {
+  digitalWrite(LED1, LOW);
+ }
+  
+}
